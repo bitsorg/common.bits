2454848b1cd9738ed7437100187149461faf7ba1
Predrag Buncic <pbuncic@yahoo.com>
2026-02-05 14:56:38 +0100

    Fixing build dependencies

--- abseil.sh
+++ abseil.sh
@@ -1,13 +1,14 @@
 package: abseil
 version: "%(tag_basename)s"
 tag: "20240722.0"
-source: https://github.com/abseil/abseil-cpp
 requires:
   - "GCC-Toolchain:(?!osx)"
 build_requires:
   - CMake
   - ninja
-  - bits-recipe-tools
+  - alibuild-recipe-tools
+  - ninja
+source: https://github.com/abseil/abseil-cpp
 prefer_system: "osx"
 prefer_system_check: |
   printf '#include <absl/container/flat_hash_map.h>' | c++ -std=c++20 -I"$(brew --prefix abseil)/include" -c -xc++ - >/dev/null
@@ -18,8 +19,19 @@ prepend_path:
   PKG_CONFIG_PATH: "$ABSEIL_ROOT/lib/pkgconfig"
 ---
 #!/bin/bash -e
-##############################
-. $(bits-include CMakeRecipe)
-##############################
-MODULE_OPTIONS="--bin --lib --cmake"
-##############################
+
+mkdir -p $INSTALLROOT
+cmake $SOURCEDIR                             \
+  -G Ninja                                   \
+  ${CXXSTD:+-DCMAKE_CXX_STANDARD=$CXXSTD}    \
+  -DCMAKE_INSTALL_LIBDIR=lib                 \
+  -DBUILD_TESTING=OFF                        \
+  -DCMAKE_INSTALL_PREFIX=$INSTALLROOT
+
+cmake --build . -- ${JOBS:+-j$JOBS} install
+
+# Modulefile
+MODULEDIR="$INSTALLROOT/etc/modulefiles"
+MODULEFILE="$MODULEDIR/$PKGNAME"
+mkdir -p "$MODULEDIR"
+alibuild-generate-module --lib --bin --cmake > "$MODULEFILE"
