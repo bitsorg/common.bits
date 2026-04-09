ac21077bcc3d004432b941f740d5df04114708bd
Predrag Buncic <pbuncic@yahoo.com>
2026-04-09 14:40:32 +0200

    Testing memory limits

--- root.sh
+++ root.sh
@@ -2,6 +2,8 @@ package: ROOT
 version: "%(tag_basename)s"
 tag: "v6-36-04"
 source: https://github.com/root-project/root.git
+mem_per_job: 1500
+mem_utilisation: 0.80
 requires:
   - abseil
   - arrow
