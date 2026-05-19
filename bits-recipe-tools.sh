8956cc51bbcb95c2dbbfe359c702903615a1a9ef
Predrag Buncic <pbuncic@yahoo.com>
2026-05-19 12:37:12 +0200

    Fix exit code from Run()

--- bits-recipe-tools.sh
+++ bits-recipe-tools.sh
@@ -1,6 +1,6 @@
 package: bits-recipe-tools
-version: "0.0.16"
-tag: "v0.0.16"
+version: "0.0.17"
+tag: "v0.0.17"
 source: https://github.com/bitsorg/bits-recipe-tools
 ---
 mkdir -p $INSTALLROOT/bin
