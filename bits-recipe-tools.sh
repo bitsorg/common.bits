4a6d74a3ed9e5484c8193f228e186763e7b24e7d
Predrag Buncic <pbuncic@yahoo.com>
2026-05-22 17:13:06 +0200

    Updating version

--- bits-recipe-tools.sh
+++ bits-recipe-tools.sh
@@ -1,6 +1,6 @@
 package: bits-recipe-tools
-version: "0.0.18"
-tag: "v0.0.18"
+version: "0.0.19"
+tag: "v0.0.19"
 source: https://github.com/bitsorg/bits-recipe-tools
 ---
 mkdir -p $INSTALLROOT/bin
